e30cf05acb6b814c8831a31f4f939134fc813696
@ndragogo´s formacion <78384046+ANTONIOa7o@users.noreply.github.com>
2023-03-10 14:14:14 +0100

    Add files via upload

---
+++ DESARROLLA _ANGULAR/1-ANGULAR_ESTRUCTURA/ejercicio2-AngularWorld/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',//donde
+  templateUrl: './app.component.html',//que
+  styleUrls: ['./app.component.css']//como
+})
+export class AppComponent {
+  title = 'DESARROLLADO POR CfC A7o';
+  titulo:string ='CURSO DE PROGRAMACION EN TECNOLOGIA MOVIL'
+
+}
---
+++ DESARROLLA _ANGULAR/1-ANGULAR_ESTRUCTURA/ejercicio2-AngularWorld/src/app/principal/principal.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-principal',
+  templateUrl: './principal.component.html',
+  styleUrls: ['./principal.component.css']
+})
+export class PrincipalComponent implements OnInit {
+
+nombre:string = 'El mundo de Angular'
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
